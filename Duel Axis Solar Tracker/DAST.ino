7b2ebd654cbfe8cc784af6f2c6de7a4b49f84d87
TejusReddy19 <109446973+TejusReddy19@users.noreply.github.com>
2024-06-13 17:11:27 +0530

    Update DAST.ino

--- Duel Axis Solar Tracker/DAST.ino
+++ Duel Axis Solar Tracker/DAST.ino
@@ -17,13 +17,10 @@ void setup() {
   myservo1.write(ser1);
   myservo2.write(100);
   lcd.begin(16, 2);
-  
-  
 }
 
 void loop() 
 {
-
   rRDL1 = analogRead(LDR1) / 100;
   rRDL2 = analogRead(LDR2) / 100;
   rRDL3 = analogRead(LDR3) / 100;
@@ -33,24 +30,17 @@ void loop()
   max2 = max(rRDL3, rRDL4);
   max3 = max(max1, max2);
   
-  //Serial.println(String(max3));
-  //Serial.println(String(rRDL1) +", "+String(rRDL2) +", "+String(rRDL3) +", "+String(rRDL4));
-
-
   if(rRDL1<max3 && rRDL2<max3)
   {
     if(ser1<140)
       ser1+=1;
     myservo1.write(ser1);
-   
-    
   }
   if(rRDL3<max3 && rRDL4<max3)
   {
     if(ser1>0)
       ser1-=1;
     myservo1.write(ser1);
-    
   }
 
   if(rRDL2<max3 && rRDL3<max3)
@@ -59,7 +49,6 @@ void loop()
     if(ser2<180)
       ser2+=1;
     myservo2.write(ser2);
-    
   }
   if(rRDL1<max3 && rRDL4<max3)
   {
@@ -67,13 +56,9 @@ void loop()
     if(ser2>0)
       ser2-=1;
     myservo2.write(ser2);
-    
-    
   }
    lcd.print("Charging.......");
    delay(1000);
    lcd.clear();
    delay(15);
-
- 
 }
